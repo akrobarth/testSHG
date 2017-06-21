5db37e162494caacb939ab3fb458392c8338248b
akrobarth <akrobarth@gmail.com>
2017-06-21 19:27:56 +0200

    feat(app): fix percent on charts

--- app/controller.js
+++ app/controller.js
@@ -9,34 +9,14 @@
 
   angular
     .module('app')
-    .controller('MainController', MainController);
+    .controller('MainController', function ($scope){
+		$scope.showMenu = false;
 
-  MainController.$inject = [];
-
-
-  function MainController() {
-
-    var self = this
-    var i = 0,
-	degs = 75,
-	activeBorder = document.getElementsByClassName('container__data__charts__chart--progression-border'),
-	active = document.getElementsByClassName('container__data__charts__chart--progression'),
-	borderstyle;
-
-	var launchBorder = function( event ) {
-		if (degs >= 50){
-			for(i=0; i < degs+1; i++){
-				borderStyle = 'background-image: linear-gradient('+ (i*1.8) +'deg, transparent 50%, #A2ECFB 50%),'+
-				'linear-gradient(90deg, #A2ECFB 50%, transparent 50%);';
-		        //setTimeout(function() {activeBorder[0].setAttribute('style', borderStyle); console.log('in')}, i * 1000);
-		        activeBorder[0].setAttribute('style', borderStyle);
-			}
-		}
-	    
-	} 
-
-	launchBorder();   
-  }
+    	
+		$scope.openMenu =function () {
+			$scope.showMenu = $scope.showMenu ? false : true;
+		};
 
 
+    });
 })();
