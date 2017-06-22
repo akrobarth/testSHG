b87ada3c0137d4e181fa80ea6d4f690848c0eda8
akrobarth <akrobarth@gmail.com>
2017-06-22 20:03:57 +0200

    fix(app): refacto data call and wheel style

--- app/app.js
+++ app/app.js
@@ -1,15 +1,30 @@
 ;(function() {
-
   /**
    * Definition of the main app module and its dependencies
    */
+
   angular
-    .module('app', ['ngRoute'])
+    .module('app', ['ngRoute','ngAnimate'])
     .config(function($routeProvider) {
       $routeProvider
       .when("/", {
           templateUrl : "views/home.html",
-          controller: 'MainController'
+          controller: 'MainController',
+          resolve: {
+            resolvedData: function(appService){
+              return appService.get();
+            }
+          }
       })
-    });
+    })
+    .service('appService', function($http){
+      return {
+        get: function(){
+          return $http.get('js/data.js').then(function(response) {
+            return response.data;
+          });
+        }
+      };
+
+    })
 })();
--- app/controller.js
+++ app/controller.js
@@ -9,18 +9,48 @@
 
   angular
     .module('app')
-    .controller('MainController', function ($scope){
+    .controller('MainController', function ($scope, resolvedData){
+
+    	var self = this; 
+
 		$scope.showMenu = false;
-		$scope.users = data;
-		$scope.userActiv = 1;
-    	
+		$scope.showdata = false;
+		$scope.users = resolvedData;
+		$scope.userActiv = $scope.users[0];
+
 		$scope.openMenu = function () {
-			$scope.showMenu = $scope.showMenu ? false : true;
+			$scope.showMenu = !$scope.showMenu;
 		};
-		$scope.changeUser = function (data){
-			$scope.userActiv = data;
+		$scope.changeUser = function (index){
+			$scope.userActiv = $scope.users[index];
 			$scope.showMenu = false;
+			$scope.showdata = false;
+			document.getElementById('datas').scrollIntoView({behavior: "smooth"});
 		};
+
+		$scope.elementVisible = function (el){
+
+			let top = el.offsetTop;
+			let height = el.offsetHeight;
+			let bottom = top + height;
+			let IsOverBottom = top > (window.pageYOffset + window.innerHeight);
+			let IsBeforeTop = bottom < window.pageYOffset;
+
+			if (!IsOverBottom && !IsBeforeTop) {
+				$scope.showdata = true;
+			}else{
+				$scope.showdata = false;
+			}
+			$scope.$apply();
+		}
+
+		$scope.onScroll = function (){
+		  for (var item of document.querySelectorAll(".container__data__charts__chart")) {
+		    	$scope.elementVisible(item);
+		  }
+		}
+
+		window.addEventListener("scroll", $scope.onScroll);
 
     });
 })();
